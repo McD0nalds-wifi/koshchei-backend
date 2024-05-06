f689bb137030ada43764dd731678d8396ec239db
mzelinov <mzelinov@ourgapps.com>
2024-05-06 21:36:17 +0300

    Added tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
+import { TaskModule } from './task/task.module';
 
 @Module({
-    imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+    imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule],
     // controllers: [],
     // providers: [],
 })
